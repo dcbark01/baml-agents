fc7cd791a311ab670541f816e815ea7153f1c978
Elijas <4084885+Elijas@users.noreply.github.com>
2025-04-25 08:57:38 +0300

    chore(bump_version): 0.16.17 → 0.16.18

--- baml_agents/__init__.py
+++ baml_agents/__init__.py
@@ -16,7 +16,7 @@ from baml_agents._jupyter_baml._jupyter_chat_widget import (
 )
 from baml_agents._project_utils._init_logging import init_logging
 
-__version__ = "0.16.17"
+__version__ = "0.16.18"
 __all__ = [
     "Action",
     "ActionRunner",
