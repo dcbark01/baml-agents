0cb3cc227d1520956fb5d3d7d9ad0d0ce2d0afe5
Elijas <4084885+Elijas@users.noreply.github.com>
2025-04-25 07:17:55 +0300

    Bump version: 0.14.10 → 0.14.11

--- baml_agents/__init__.py
+++ baml_agents/__init__.py
@@ -16,7 +16,7 @@ from baml_agents._jupyter_baml._jupyter_chat_widget import (
 )
 from baml_agents._project_utils._init_logging import init_logging
 
-__version__ = "0.14.10"
+__version__ = "0.14.11"
 __all__ = [
     "Action",
     "ActionRunner",
