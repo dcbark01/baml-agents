b8db1023d103987d709696cf6ca07580cc98680a
Elijas <4084885+Elijas@users.noreply.github.com>
2025-05-08 15:16:10 +0300

    feat: add gemini prompot getter

--- baml_agents/_agent_tools/_utils/_baml_utils.py
+++ baml_agents/_agent_tools/_utils/_baml_utils.py
@@ -6,16 +6,19 @@ T = TypeVar("T")
 
 
 def get_prompt(request):
-    messages = request.body.json()["messages"]
-    prompt_parts = []
-    for message in messages:
-        content = sole(message["content"])
-        if content["type"] != "text":
-            raise ValueError(
-                f"Expected content type 'text', but got '{content['type']}'",
-            )
-        prompt_parts.append(f"[{message['role']}]\n{content['text']}")
-    return "\n\n".join(prompt_parts)
+    try:
+        return sole(sole(request.body.json()["contents"])["parts"])["text"]
+    except KeyError as e:
+        messages = request.body.json()["messages"]
+        prompt_parts = []
+        for message in messages:
+            content = sole(message["content"])
+            if content["type"] != "text":
+                raise ValueError(
+                    f"Expected content type 'text', but got '{content['type']}'",
+                ) from e
+            prompt_parts.append(f"[{message['role']}]\n{content['text']}")
+        return "\n\n".join(prompt_parts)
 
 
 def display_prompt(request):
