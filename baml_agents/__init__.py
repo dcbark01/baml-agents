39845a1e9321bb1e157bc46ab10936d88eaf731e
Elijas <4084885+Elijas@users.noreply.github.com>
2025-05-22 12:13:36 +0300

    chore(release): bump version from 0.39.0 to 0.40.0

--- baml_agents/__init__.py
+++ baml_agents/__init__.py
@@ -52,7 +52,7 @@ from baml_agents._project_utils._init_logging import init_logging
 from baml_agents._utils._must import must
 from baml_agents._utils._sole import sole
 
-__version__ = "0.39.0"
+__version__ = "0.40.0"
 __all__ = [
     "Action",
     "ActionRunner",
